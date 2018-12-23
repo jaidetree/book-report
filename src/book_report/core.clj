a4404c9e1e3c1d8fa9e1638829a724e0f1e40650
Jay Zawrotny <jayzawrotny@gmail.com>
2018-12-23 01:10:54 -0500

    Completed first pass of required functionality

--- src/book_report/core.clj
+++ src/book_report/core.clj
@@ -2,6 +2,18 @@
   (:require [clojure.string :as string]
             [clojure.pprint :refer [pprint]]))
 
+(defn format-return
+  [item list]
+  (if (> (count list) 1)
+    (str "      " item)
+    (str item)))
+
+(defn append
+  ([item list]
+   (append identity item list))
+  ([f item list]
+   (conj (vec list) (f item list))))
+
 (defn format-note
   [[note & lines]]
   (cons note
@@ -13,7 +25,9 @@
   (->> notes
        (map #(str "   - " (string/trim %)))
        (mapcat #(format-note (string/split % #"\n")))
-       (string/join "\n")))
+       (string/join "\n")
+       (conj `[println "  Notes:\n"])
+       (seq)))
 
 (defn format-code-lines
   [lines]
@@ -35,13 +49,9 @@
        (map string/trim)
        (map #(format-code-lines (string/split % #"\n")))
        (reduce space-code-blocks "")
-       (string/trimr)))
-
-(defn append
-  [item list]
-  (concat list [(if (> (count list) 1)
-                  (str "      " item)
-                  (str item))]))
+       (string/trimr)
+       (conj `[println])
+       (seq)))
 
 (defmacro with-out-str-and-value
   [& body]
@@ -50,71 +60,131 @@
        (let [v# ~@body]
          [(str s#) v#]))))
 
+(defn run-code
+  [forms]
+  `(do ~@forms))
+
 (defn eval-str
   [form]
   (with-out-str-and-value (eval form)))
 
 (defn format-eval
   [forms]
-  (let [[output return-value] (eval-str forms)
+  (let [[output return-value] (eval-str `(do ~@forms))
         [first-line & lines] (string/split output #"\n")]
     (->> lines
          (map string/trim)
          (map #(str "      " %))
          (cons first-line)
-         (append (pr-str return-value))
+         (append format-return (pr-str return-value))
          (remove empty?)
-         (string/join "\n"))))
+         (string/join "\n")
+         (conj `[println "   ➜"])
+         (seq))))
+
+(defn format-title
+  [titles]
+  (let [title (apply str "  # " titles)
+        underline (apply str "\n  " (repeat (count title) "–"))
+        title (str title underline)]
+    `(println ~title)))
+
+(comment
+ (eval (format-title "Hello World")))
+
+(defn op?
+  [form]
+  (if (seq? form)
+    (not (contains? '#{::value notes run title} (first form)))
+    true))
 
 (defmacro lesson
   "Render code and its output grouped as a lesson from a chapter.
   Takes a section-id number, title string, notes, and code forms.
   Returns a list of expressions to display it nicely.
-  (lesson 1.1
-          \"How does add work?\"
-          \"Should return 2\"
+  (lesson 1
+          \"Calling functions\"
+          (title \"Addition example\")
+          (notes \"Should return 2\")
           (+ 1 1))
   "
   [section-id title & forms]
-  (if (= 'notes (-> forms first first))
-    (let [notes (-> forms first rest (format-notes))
-          forms (rest forms)
-          code (format-code forms)]
-      `(do (println (str "Chapter " ~section-id " :: " ~title))
-           (println "\n  Notes:")
-           (println ~notes)
-           (println "")
-           (println ~code)
-           (print "   => ")
-           (println (format-eval '(do ~@forms)))
-           (println "\n")))
-    (let [code (format-code forms)]
-      `(do (println (str "Chapter " ~section-id " :: " ~title))
-           (println ~code)
-           (print "   => ")
-           (println (format-eval '(do ~@forms)))
-           (println "\n")))))
+  (loop [forms forms
+         output `[(println (str "Chapter " ~section-id " :: " ~title))]]
+    (let [append-line #(apply conj output (conj %& `(println "")))
+          form (first forms)
+          remaining (rest forms)
+          [form-head & form-args] (if (seq? form)
+                                    form
+                                    (list ::value form))]
+      (cond (nil? form) `(do ~@(seq (conj output `(println ""))))
+            (= form-head ::value) (recur remaining
+                                         (apply append-line form-args))
+            (= form-head 'notes)  (recur remaining
+                                         (append-line
+                                          (format-notes form-args)))
+            (= form-head 'run)    (do
+                                    (eval `(do ~@form-args))
+                                    (recur remaining output))
+
+            (= form-head 'title) (recur remaining
+                                        (append-line (format-title form-args)))
+            :else
+              ;; Scoop up all forms between one for the special case forms
+              ;; above
+              (let [[eval-forms remaining] (split-with op? forms)]
+                (recur remaining
+                       (append-line (format-code eval-forms)
+                                    (format-eval eval-forms))))))))
 
 (comment
-  (macroexpand
-   '(lesson 1
-          "This is a test lesson"
-          (notes  "Should return 2")
-          (+ 1 1)))
-  (lesson 1
-          "This is a test lesson"
-          (notes  "Should return 2")
-          (+ 1 1))
-  (lesson 2
-          "This is a test lesson"
+   (macroexpand
+    '(lesson 1
+             "This is a test lesson"
+             (notes  "Should return 2")
+             (+ 1 1)))
+   (lesson 0
+          "Calling functions"
+          (title "Add")
           (notes "Should return 2")
-          (+ 1 1)
-          (+ 2 3))
-  (lesson 3
-          "This is a test lesson"
           (+ 1 1))
-  (lesson 4
-          "This is a test lesson"
-          (notes  "Should return 2
+   (lesson 1
+           "This is a test lesson"
+           (notes  "Should return 2")
+           (+ 1 1))
+   (lesson 2
+           "This is a test lesson"
+           (notes "Should return 2")
+           (+ 1 1)
+           (+ 2 3))
+   (lesson 3
+           "This is a test lesson"
+           (+ 1 1))
+   (lesson 4
+           "This is a test lesson"
+           (notes  "Should return 2
                    Plus another note")
-          (+ 1 1)))
+           (+ 1 1))
+   (lesson 5
+           "This is a test lesson"
+           (notes  "Should return 2")
+           (+ 1 1))
+   (lesson 6
+           "This is a test lesson"
+           (notes "Should return 2")
+           (+ 1 3)
+           (notes "x")
+           2)
+   (lesson 7
+           "This is a test lesson"
+           (notes "Should return 2")
+           (run (defn add [x] (+ x 3)))
+           (notes "x")
+           (add 3))
+   (lesson 8
+           "This is a test lesson"
+           (notes "Should return 2")
+           (run (defn add [x] (+ x 3)))
+           (title "Look at x")
+           (notes "x")
+           (add 3)))

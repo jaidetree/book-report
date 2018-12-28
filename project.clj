1b967690fa1201284305ea8d51a422ed4b07543b
Jay Zawrotny <jayzawrotny@gmail.com>
2018-12-27 22:12:42 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject book-report "0.1.0-SNAPSHOT"
+(defproject book-report "0.1.0"
   :description "A Clojure library to ease learning Clojure from books."
   :url "https://github.com/jayzawrotny/book-report"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
